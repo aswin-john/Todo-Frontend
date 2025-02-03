bff9121e304ad432ae3774d80a1fd106ea24d79e
Aswin John <aswinjohnofficial478@gmail.com>
2025-02-04 00:15:48 +0530

    Add navigation dependencies and implement navigation structure in App component

--- App.jsx
+++ App.jsx
@@ -1,23 +1,20 @@
 import React from 'react'
 import Login from './Components/Login/Login'
 import SignUp from './Components/SignUp/SignUp'
-// import { NavigationContainer } from '@react-navigation/native'
-// import { createNativeStackNavigator } from '@react-navigation/native-stack'
-
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 function App() {
   const stack = createNativeStackNavigator()
   return (
-    <Login />
+    // <Login />
     // <SignUp/>
-    // <NavigationContainer>
-    //   <stack.Navigator>
-    //     <stack.Screen name="Login" component={Login} />
-    //     <stack.Screen name="SignUp" component={SignUp} />
-    //   </stack.Navigator>
-    // </NavigationContainer>
+    <NavigationContainer>
+      <stack.Navigator>
+        <stack.Screen name="Login" component={Login} />
+        <stack.Screen name="SignUp" component={SignUp} />
+      </stack.Navigator>
+    </NavigationContainer>
   )
 }
 
